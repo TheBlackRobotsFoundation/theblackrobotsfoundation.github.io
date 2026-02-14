2cd83b76e0e2c8aab54451d18d791c43a88282f3
Diego <diegorayado1212@gmail.com>
2026-02-14 20:58:52 +0100

    Corregir enlace del botón Ver Rover a rover-web

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--primary button--lg"
-            to="/proyectos/rover/">
+            to="/proyectos/rover-web/">
             Ver Rover
           </Link>
         </div>
